c60411dfd6ab24edff8f8e1b6167f2c04f03682b
EdwinDij <edwin.d899@gmail.com>
2022-10-09 12:21:10 +0200

    fin du projet csv crée

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+title = []
+price = []
 for i in range (1, 51):
     url1 = "http://books.toscrape.com/catalogue/page-"+ str(i)+".html"
-    print(url1)
     page = requests.get(url1)
     soup = BeautifulSoup(page.content, "html.parser")
     all_title = soup.find_all("h3")
-    title = []
+    all_price = soup.find_all('p', class_="price_color")
     for titles_sc in all_title:
             title.append((str(titles_sc.find('a')).split('title=', 1)[1]).split('>', 1)[0])
-            print(title)
+    for price_sc in all_price:
+            price.append(price_sc.string)
+
+header = ['titre du livre', 'prix du livre']
+with open('data.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(header)
+    for titre, prix in zip(title, price):
+        writer.writerow([titre, prix])
